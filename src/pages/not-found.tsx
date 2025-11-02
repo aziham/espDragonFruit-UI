08facd30d2f36b9968b8bee4267131989bbef7db
aziham <hamza.aziane@pm.me>
2025-11-02 05:01:25 +0100

    fix: Redirect to /404 safely when route isn't found

--- src/pages/not-found.tsx
+++ src/pages/not-found.tsx
@@ -1,3 +1,13 @@
+import { useEffect } from 'preact/hooks';
+import { useLocation } from 'wouter-preact';
+
 export function NotFoundPage() {
+  const [location, navigate] = useLocation();
+  const notFoundUrl = '/404';
+
+  useEffect(() => {
+    if (location !== notFoundUrl) navigate(notFoundUrl);
+  }, [location, navigate]);
+
   return <div>404!</div>;
 }
